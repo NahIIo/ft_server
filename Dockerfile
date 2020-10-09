4b34cc0610ffe956085731f471ed69666054984b
Julian Chauvet <Julian@Yes.local>
2020-10-09 15:30:44 +0200

    works

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ LABEL maintainer="Julian CHAUVET <jchauvet@student.42.fr"
 
 RUN apt-get update && apt-get -y install dialog apt-utils vim wget openssl
 
-RUN apt-get -y install php7.3-fpm php7.3-mysql php7.3-common php7.3-gmp php7.3-curl php7.3-intl php7.3-mbstring php7.3-xmlrpc php7.3-gd php7.3-xml php7.3-cli php7.3-zip php7.3-soap php7.3-imap
+RUN apt-get -y install php7.3-fpm php7.3-mysql php7.3-common php7.3-gmp php7.3-curl php7.3-intl php7.3-mbstring php7.3-xmlrpc php7.3-gd php7.3-xml php7.3-cli php7.3-zip php7.3-soap php7.3-imap zip
 
 RUN apt-get install nginx -y
 
@@ -14,5 +14,5 @@ EXPOSE 80 443
 
 COPY srcs /tmp/
 
-CMD bash /tmp/setup.sh && tail -f /dev/null
+CMD bash /tmp/setup.sh
 
--- srcs/setup.sh
+++ srcs/setup.sh
@@ -1,13 +1,9 @@
 #!bin/bash
+
 chown -R www-data /var/www/*
 chmod -R 755 /var/www/*
 
 mkdir /var/www/localhost
-
-####
-
-mkdir /etc/nginx/ssl
-openssl req -x509 -newkey rsa:4096 -days 9999 -keyout key.pem -nodes -out /etc/nginx/ssl/cert.pem -keyout /etc/nginx/ssl/ssl.key -subj "/"
 
 ####
 
@@ -29,18 +25,33 @@ EOF
 mkdir /var/www/localhost/phpmyadmin
 wget https://files.phpmadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
 tar -xvf phpMyadmin-4.9.0.1-all-languages.tar.gz --strip-components 1 -C /var/www/localhost/phpmadmin
-mv /tmp/phpmyadmin.conf /var/www/localhost/phpmyadmin/config.inc.php
+mv /tmp/phpmyadmin.php /var/www/localhost/phpmyadmin/config.inc.php
 
 ####
 
-wget -c https://wordpress.org/latest.tar.gz
-tar -xzvf latest.tar.gz
-mv wordpress/ /var/www/localhost/
+cd /tmp/
+wget https://wordpress.org/latest.tar.gz
+tar -xvzf latest.tar.gz
+mv wordpress/ /var/www/localhost/wordpress
 mv /tmp/wp-config.php /var/www/localhost/wordpress
+
 
 ####
 
 service php7.3-fpm start
+
+####
+
+mkdir ~/mkcert
+cd ~/mkcert
+wget https://github.com/FiloSottile/mkcert/releases/download/v1.1.2/mkcert-v1.1.2-linux-amd64
+mv mkcert-v1.1.2-linux-amd64 mkcert
+chmod +x mkcert
+./mkcert -install
+./mkcert localhost
+
+####
+
 service nginx restart
 
 # docker build -t ft_server .
