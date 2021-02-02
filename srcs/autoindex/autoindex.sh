b6a35a4b002302121c633cf8eb26194f6ccd28e2
Julian Chauvet <Julian@Yes.local>
2021-02-02 12:48:39 +0100

    Fixed autoindex and removed HSTS

--- srcs/autoindex/autoindex.sh
+++ srcs/autoindex/autoindex.sh
@@ -6,7 +6,7 @@
 #    By: Julian <Julian@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/01/21 09:55:43 by Julian            #+#    #+#              #
-#    Updated: 2021/01/28 12:56:41 by Julian           ###   ########.fr        #
+#    Updated: 2021/02/02 12:47:35 by Julian           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,9 +17,7 @@ echo "Please enter what mode you want autoindex to be on: On/Off"
 read result
 if [[ $result == $'On' || $result == $'on' ]]
 then
-
 	test=$(grep -c "autoindex on" /etc/nginx/sites-available/localhost)
-
 	if [[ $test == 1 ]]
 	then
 		echo "Autoindex already on!"
@@ -31,7 +29,6 @@ then
 elif [[ $result == $'Off' || $result == $'off' ]]
 then
 	test=$(grep -c "autoindex off" /etc/nginx/sites-available/localhost)
-
 	if [[ $test == 1 ]]
 	then
 		echo "Autoindex already off!"
@@ -41,8 +38,6 @@ then
 		service nginx restart
 	fi
 else
-
 	echo "Invalid Input!"
-
 fi
 exit
